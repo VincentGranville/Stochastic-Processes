218c0c3aae752277b02b04766e2742bbdfc80e33
Vincent Granville <vincentg@mltechniques.com>
2023-05-01 21:52:50 -0700

    Recursion to produce the winning numbers in the lottery system

--- lottery.py
+++ lottery.py
@@ -1,26 +1,32 @@
-# z is an 8-bit winning number if t >= T and (t - T) % 8 == 0
+# w is an 8-bit winning number if t >= T and (t - T) % 8 == 0
 
-x = 1          # seed: 1st component, x0
-y = 2          # seed: 2nd component, y0
-T = 53         # must be >= 10
+t = 0
+y = 2   # 1        # seed: 1st component, y0 (for t = 0)
+z = 5   # 2        # seed: 2nd component, z0 (for t = 0)
+T = 43         # must be >= 10
 max = 200      # maximum for t, must be >= T
-buffer = {}    # to store 9 previous values of x
+buffer = {}    # to store 9 previous vales of x
+x0 = 0         # irrational number represented by the digits 
 
-for t in range(2, max): 
+for t in range(1, max): 
 
-    if 4*x + 1 < 2*y:
-        y = 4*y - 8*x - 2
-        x = 2*x + 1
+    if z < 2*y:
+        y = 4*y - 2*z
+        z = 2*z + 3
         d = 1        # t-th binary digit (unused)  
     else:
-        x = 2*x
         y = 4*y
+        z = 2*z - 1
         d = 0        # t-th binary digit (unused)
+  
+    x0 += d * 1/2**t 
 
     if t >= T - 8:
-      buffer[t % 9] = x
+      buffer[t % 9] = z
       if t >= T:
-          z = x - 256*buffer[(t-8) % 9] 
+          w = (z - 256*buffer[(t-8) % 9] + 255) >> 2  
 
     if t >= T and (t - T) % 8 == 0:
-        print(t, z, x) 
+        print(t, w, d, z) 
+
+print("\nNumber x0:", x0)
